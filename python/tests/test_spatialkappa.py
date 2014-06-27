fc8d2066491dbdf8e02d2c6a30c97c1e558fd876
David C Sterratt <david.c.sterratt@ed.ac.uk>
2014-06-27 17:30:18 +0100

    More tests for addAgent()

--- python/tests/test_spatialkappa.py
+++ python/tests/test_spatialkappa.py
@@ -1,6 +1,8 @@
 import SpatialKappa
 import os
 import unittest
+from py4j.protocol import * 
+import random
 
 class TestSpatialKappa(unittest.TestCase):
     
@@ -11,8 +13,6 @@ class TestSpatialKappa(unittest.TestCase):
 
     def test_runForTime(self):
         self.sim.runForTime(100.0, False)
-        obs = self.sim.getObservation("ca")
-        self.assertTrue(isinstance(obs, float))
         self.assertEqual(self.sim.getTime(), 100.0)
 
     def test_addTransition(self):
@@ -21,6 +21,29 @@ class TestSpatialKappa(unittest.TestCase):
         self.assertEqual(str(tran), "'TEST' : [] -> [[ca(x)]] @ 0.0")
         self.sim.setTransitionRate("TEST", 100.0)
         self.assertEqual(str(tran), "'TEST' : [] -> [[ca(x)]] @ 100.0")
+
+    def test_addAgent(self):
+        obs = self.sim.getObservation("ca")
+        self.assertTrue(isinstance(obs, float))
+        ## Add an integer number of Ca ions specified as a float
+        self.sim.addAgent('ca', 11.0) 
+        obs2 = self.sim.getObservation("ca")
+        self.assertEqual(obs2, obs + 11.0)
+        ## Add an integer number of Ca ions specified as an integer
+        self.sim.addAgent('ca', 10) 
+        obs3 = self.sim.getObservation("ca")
+        self.assertEqual(obs3, obs2 + 10)
+
+        ## Somthing like this should work, but it doesn't.
+        ## self.assertRaises(TypeError, random.shuffle, (1,2,3))
+        ## Hence the hack below
+        error = False
+        try:
+            self.sim.addAgent("ca", 11.5)
+        except Py4JJavaError:
+            error = True
+        self.assertTrue(error)
+
 
 if __name__ == '__main__':
     unittest.main()
