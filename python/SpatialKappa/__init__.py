2459f8aa04efc40110ab3fd14320d128d6c21019
David C Sterratt <david.c.sterratt@ed.ac.uk>
2014-07-03 19:22:19 +0100

    Kill gateway process from destructor rather than using die_on_exit
    This is because with my hack to py4j the die_on_exit method doesn't
    work because of the redirection of the stdout. In any case, this is
    probably a neater method.

--- python/SpatialKappa/__init__.py
+++ python/SpatialKappa/__init__.py
@@ -15,7 +15,7 @@ class SpatialKappa:
         antlrjar_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'share', 'SpatialKappa', antlrjar_file)
         classpath = os.pathsep.join((classpath, skjar_path, antlrjar_path))
         print classpath
-        self.port = jg.launch_gateway(classpath=classpath, die_on_exit=True)
+        self.port = jg.launch_gateway(classpath=classpath, die_on_exit=False)
         self.gateway_client = jg.GatewayClient(port=self.port)
         self.gateway = jg.JavaGateway(self.gateway_client, auto_convert=True)        
 
@@ -23,4 +23,6 @@ class SpatialKappa:
         ks = self.gateway.jvm.org.demonsoft.spatialkappa.api.SpatialKappaSim(time_units, verbose)
         return ks
 
-
+    def __del__(self):
+        self.gateway_client.shutdown_gateway()
+        
