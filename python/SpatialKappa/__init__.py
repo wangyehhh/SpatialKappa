b41e6ff98b8fb8e55fc952054e238cabae3959ac
David C Sterratt <david.c.sterratt@ed.ac.uk>
2014-08-06 17:19:05 +0100

    Get rid of printing classpath

--- python/SpatialKappa/__init__.py
+++ python/SpatialKappa/__init__.py
@@ -14,10 +14,9 @@ class SpatialKappa:
         skjar_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'share', 'SpatialKappa', skjar_file)
         antlrjar_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'share', 'SpatialKappa', antlrjar_file)
         classpath = os.pathsep.join((classpath, skjar_path, antlrjar_path))
-        print classpath
         self.port = jg.launch_gateway(classpath=classpath, die_on_exit=False)
         self.gateway_client = jg.GatewayClient(port=self.port)
-        self.gateway = jg.JavaGateway(self.gateway_client, auto_convert=True)        
+        self.gateway = jg.JavaGateway(self.gateway_client, auto_convert=True)
 
     def kappa_sim(self, time_units, verbose):
         ks = self.gateway.jvm.org.demonsoft.spatialkappa.api.SpatialKappaSim(time_units, verbose)
