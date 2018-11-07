071ebec8c83d2629670a7f78b3d80155f148aedf
David C Sterratt <david.c.sterratt@ed.ac.uk>
2018-11-07 17:00:58 +0000

    Allow output of SpatialKappa python to be redirected with redirect_output

--- python/SpatialKappa/__init__.py
+++ python/SpatialKappa/__init__.py
@@ -9,14 +9,19 @@ class SpatialKappa:
     port = None
     gateway = None
     gateway_client = None
+    count = 0
+    
+    def __init__(self, classpath='', redirect_stdout=None):
 
-    def __init__(self, classpath=''):
         skjar_file = 'SpatialKappa-v2.1.1.jar'
         antlrjar_file = 'ant-antlr-3.2.jar'
         skjar_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'share', 'SpatialKappa', skjar_file)
         antlrjar_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'share', 'SpatialKappa', antlrjar_file)
         classpath = os.pathsep.join((classpath, skjar_path, antlrjar_path))
-        self.port = jg.launch_gateway(classpath=classpath, die_on_exit=False)
+        self.id = SpatialKappa.count
+        SpatialKappa.count = SpatialKappa.count + 1
+        self.redirect_stdout = redirect_stdout
+        self.port = jg.launch_gateway(classpath=classpath, die_on_exit=False, redirect_stdout=self.redirect_stdout)
         self.gateway_client = jg.GatewayClient(port=self.port)
         self.gateway = jg.JavaGateway(self.gateway_client, auto_convert=True)
 
@@ -26,4 +31,5 @@ class SpatialKappa:
 
     def __del__(self):
         self.gateway_client.shutdown_gateway()
-        
+        SpatialKappa.count = SpatialKappa.count - 1
+
